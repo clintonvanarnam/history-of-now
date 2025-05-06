40032f9e4d3a49d2d94f93b711574c1525e8ab2b
clintonvanarnam <info@clintonvanarnam.net>
2025-05-06 14:35:14 -0400

    updated

--- antikythera.js
+++ antikythera.js
@@ -11,6 +11,20 @@ window.process = {
     const api = new Antikythera({
       slug: "historyofnow" 
     });
-  
+
+    const u = api.getData(); // Assuming u is fetched from the API
+    const A = [];
+    if (Array.isArray(u)) {
+      u.forEach((w) => {
+        w.scanText.enableScanText && w.scanText.scanSegments.forEach((N) => {
+          A.push({
+            id: w.id,
+            keyword: N.scanKeyword,
+            segment: N
+          });
+        });
+      });
+    }
+
     console.log("Antikythera initialized:", api);
   });
