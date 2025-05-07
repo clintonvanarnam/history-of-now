46b4a3024f7d1caa8419d19d051805740657ca6a
Andrey Karabanov <35742330+regretware@users.noreply.github.com>
2025-05-07 19:05:05 +0200

    Set correct index.html, reimplemented sticker in accordance with latest docs

--- antikythera.js
+++ antikythera.js
@@ -1,30 +1,33 @@
 // antikythera.js
 window.process = {
-    env: {
-      NODE_ENV: "production"
-    }
-  };
-  
-  window.addEventListener("DOMContentLoaded", async () => {
-    const { Antikythera } = await import('@antikythera/antikythera');
-  
-    const api = new Antikythera({
-      slug: "historyofnow" 
-    });
+  env: {
+    NODE_ENV: "production",
+  },
+};
 
-    const u = api.getData(); // Assuming u is fetched from the API
-    const A = [];
-    if (Array.isArray(u)) {
-      u.forEach((w) => {
-        w.scanText.enableScanText && w.scanText.scanSegments.forEach((N) => {
+window.addEventListener("DOMContentLoaded", async () => {
+  const { Antikythera } = await import(
+    "./node_modules/@antikythera/antikythera/dist/antikythera.js"
+  );
+
+  const api = new Antikythera({
+    slug: "historyofnow",
+  });
+
+  const u = api.getData(); // Assuming u is fetched from the API
+  const A = [];
+  if (Array.isArray(u)) {
+    u.forEach((w) => {
+      w.scanText.enableScanText &&
+        w.scanText.scanSegments.forEach((N) => {
           A.push({
             id: w.id,
             keyword: N.scanKeyword,
-            segment: N
+            segment: N,
           });
         });
-      });
-    }
+    });
+  }
 
-    console.log("Antikythera initialized:", api);
-  });
+  console.log("Antikythera initialized:", api);
+});
